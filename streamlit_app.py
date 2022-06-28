712ee634d33f2d9f58bfd0e8277ca60dfa9fc2dd
etvander <60985797+etvander@users.noreply.github.com>
2022-06-28 14:28:28 -0500

    Update streamlit_app.py
    Updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
-streamlit.title('My Parents New Healther Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('OMega 3')
-streamlit.text('Kale')
-streamlit.text('Egg')
+streamlit.title('My Mom''s New Healther Diner')
+streamlit.header('Breakfast Favorites
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
